88377cdb433fc79b8525bf6d3e6288061b8953a4
JunH1004 <junejune040218@naver.com>
2024-02-02 14:01:35 +0900

    enemy hp bar

--- yumyum_flutter/unity/yumyum_unity/Assets/Codes/Enemy.cs
+++ yumyum_flutter/unity/yumyum_unity/Assets/Codes/Enemy.cs
@@ -1,47 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using MoreMountains.Feedbacks;
+
 public class Enemy : MonoBehaviour
 {
     [SerializeField]
     private int hp;
+    private int maxHp = 20; // Set your maximum HP value here
     private bool isDead = false;
     GameManager gameManager;
 
     public MMF_Player getDamageFeedback;
+    public Slider healthSlider; // Reference to the Slider UI component
+
     // Start is called before the first frame update
     void Start()
     {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        InitializeHealthSlider();
+    }
+
+    // Initialize the health slider with current and max HP values
+    void InitializeHealthSlider()
+    {
+        if (healthSlider != null)
+        {
+            healthSlider.maxValue = maxHp;
+            healthSlider.value = hp;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateHealthSlider();
     }
-    void GetDamage(int d) {
-        hp -= d;
+
+    // Update the health slider's value based on current HP
+    void UpdateHealthSlider()
+    {
+        if (healthSlider != null)
+        {
+            healthSlider.value = hp;
+        }
+    }
+
+    void GetDamage(int damage)
+    {
+        hp -= damage;
         if (!getDamageFeedback.IsPlaying)
         {
             getDamageFeedback.PlayFeedbacks();
         }
-        //damage feedback
-        if (hp < 0) {
+
+        // Update the health slider when taking damage
+        UpdateHealthSlider();
+
+        // Damage feedback
+        if (hp <= 0)
+        {
             hp = 0;
             isDead = true;
             Die();
         }
     }
-    void Die() {
+
+    void Die()
+    {
         gameManager.isEnemyExist = false;
         gameManager.SpawnEnemy();
-        Destroy(this.gameObject);
+        Destroy(gameObject);
     }
+
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.tag == "Bullet") {
+        if (col.tag == "Bullet")
+        {
             Destroy(col.gameObject);
             GetDamage(1);
         }
