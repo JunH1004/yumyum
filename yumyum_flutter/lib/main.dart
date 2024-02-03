e42aa253e24f01da9b5e7bd2df3e5f1e1d54d1f3
Wonjun Jeon <jwjjwj1960@naver.com>
2024-02-03 09:27:50 +0900

    최종 push

--- yumyum_flutter/lib/main.dart
+++ yumyum_flutter/lib/main.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
-import 'vision_detector_views/face_detector_view.dart';
+import 'package:yumyum_flutter/my_style.dart';
+import 'package:yumyum_flutter/ui/home_page.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  runApp(MyApp());
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: FaceDetectorView(),
+      theme: mainTheme, //mainTheme - my_style.dart
+      home: HomePage(),
     );
   }
 }
